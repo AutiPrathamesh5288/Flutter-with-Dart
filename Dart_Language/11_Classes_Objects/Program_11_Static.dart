27d92d424dc8ec2de88804ad833656f2361513ab
AutiPrathamesh5288 <prathameshauti004@gmail.com>
2023-09-25 23:44:10 +0530

    Class and Object in Dart

---
+++ Dart_Language/11_Classes_Objects/Program_11_Static.dart
@@ -0,0 +1,17 @@
+class Demo {
+	int x = 10;
+	static int y =20;
+	
+	static void printData(){
+		print(x);
+		print(y);
+	}
+}
+void main(){
+//	print(Demo.y);		
+}
+
+/*
+OUTPUT :
+Error: Undefined name 'x'.
+*/
